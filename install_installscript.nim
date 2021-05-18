7923bea7eacfc054ce3b858df4aa61d9294850e9
Privacy_Dragon <privacy_dragon@tutanota.com>
2021-05-18 17:08:42 +0200

    Add files via upload

---
+++ install_installscript.nim
@@ -0,0 +1,19 @@
+import os, strformat
+
+try:
+  createDir(expandTilde("~/.local/share/applications"))
+  createDir(expandTilde("~/.local/bin/"))
+  moveFile("local_installer", expandTilde("~/.local/bin/local_installer"))
+  moveFile("local_installer-gui", expandTilde("~/.local/bin/local_installer-gui"))
+  let shutUpNim = expandTilde("~/.local/bin/local_installer-gui")
+  let desktopfile = &"[Desktop Entry]\nName=LocalInstaller\nComment=Locally install programs\nExec=\"{shutUpNim}\""
+  let stupidnim = "\nTerminal=True\nType=Application\nCategories=Utility;"
+  let desktop = &"{desktopfile}{stupidnim}"
+  writeFile(expandTilde("~/.local/share/applications"), desktop)
+except:
+  echo "Oh no! Something went wrong!"
+
+
+
+
+
